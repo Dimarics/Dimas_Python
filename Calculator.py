f7b9da0b6d3b4b41daf886f68a9ce964e4c263f1
Dimarics <dima.sikora.97@mail.ru>
2021-06-07 16:44:16 +0300

    Update Calculator.py 0.2

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,57 @@
 # Господа, эта программа предназначена для выполнения вычислений
 
 print("Вас приветствует интерактивная программа-калькулятор")
-print("Тут вы можете избавить свою ленивую задницу от необходимости самой выполнять вычисления") 
+print("Здесь вы можете упростить свою жизнь и избавиться от необходимости самому выполнять вычисления") 
 
-user_num_one = int(input("Введите первое число "))
-operator = str(input("Введите оператор "))
-user_num_two = int(input("Введите второе число "))
-if operator == '+':
-    result = user_num_one + user_num_two
-elif operator == '-':
-    result = user_num_one - user_num_two
-elif operator == '*':
-    result = user_num_one * user_num_two
-elif operator == '/':
-    result = user_num_one / user_num_two
-else:
-    print("Ошибка ввода")
-print("Результат: ", result)
+continued = 1
+
+while continued != 'n':
+
+    user_check_one = 'false'
+    user_check_two = 'false'
+    convert = 0
+    
+    while user_check_one == 'false':    # Проверка первого числа
+        try:
+            user_num_one = float(input("Введите первое число "))
+        except ValueError:
+            print("Ошибка ввода")
+        else:
+            user_check_one = 'true'
+
+    while convert == 0:     # Проверка оператора
+        operator = str(input("Введите оператор "))
+        if operator == '+':     # Со строками логика работать не хочет, поэтому приходится делать так
+            convert = 1
+        elif operator == '-':
+            convert = 1
+        elif operator == '*':
+            convert = 1
+        elif operator == '/':
+            convert = 1
+        else:
+            convert = 0
+            print("Ошибка ввода")
+
+    while user_check_two == 'false':    # Проверка второго числа
+        try:
+            user_num_two = float(input("Введите второе число "))
+        except ValueError:
+            print("Ошибка ввода")
+        else:
+            user_check_two = 'true'
+
+    if operator == '+':     # Выполняем вычисления
+        result = user_num_one + user_num_two
+    elif operator == '-':
+        result = user_num_one - user_num_two
+    elif operator == '*':
+        result = user_num_one * user_num_two
+    else:
+        result = user_num_one / user_num_two
+
+    print()
+    print("Результат: ", result)
+    print()
+    print("Продолжить? ")
+    continued = str(input("Любой ввод - да, n - нет "))
